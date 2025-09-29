33b51a17e7ea0ccc572fb815968602493cb9fbb3
Artur-rah <ferreira.artur@escola.pr.gov.br>
2025-09-29 10:11:18 -0300

    e

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const IMG = {
   sheet: "img/Custom _ Edited - Sonic the Hedgehog Customs - Sonic the Hedgehog - Sonic.png"
 };
 
-const CELL = 64;
+const CELL = 32;
 const SCALE = 2.5;
 
 const SPRITES = {
